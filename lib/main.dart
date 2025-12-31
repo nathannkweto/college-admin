5201fd25657c09e8eefc1756f8ed3810f1d31990
Nathan Nkweto <nathannkweto@nkweto.tech>
2025-12-31 11:39:43 +0100

    chore: rid of environment file dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 import 'package:college_admin/core/services/api_service.dart';
 import 'config.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Load .env file
-  await dotenv.load(fileName: ".env");
 
   // Initialize API Service Global (Singleton)
   ApiService().init(baseUrl: Config.baseUrl);
