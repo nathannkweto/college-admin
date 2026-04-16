e32b8d4fdc8c91848fb6bd3bd44fed0c6530414a
Nathan Nkweto <nathannkweto@nkweto.tech>
2026-04-16 13:38:04 +0100

    config

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const baseUrl = String.fromEnvironment(
     'API_URL',
-    defaultValue: 'https://college-app-316955810695.us-east1.run.app',
+    defaultValue: 'https://college-app-316955810695.us-east1.run.app/api/v1',
   );
 }
